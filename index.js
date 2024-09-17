da6fa004ac4318e362b7ef8d5847bcb6d544bc61
vitoriaeduardaa <167381689+vitoriaeduardaa@users.noreply.github.com>
2024-09-17 22:52:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const prompt = require('prompt-sync')
+const entrada = prompt();
+
+const nomeCompleto = 'Vitoria Eduarda Araujo';
+const anoNascimento = '2006';
+const anoAtual = '2024';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(listaDeViagens);
+
+var nomeComprador = entrada(" qual é seu nome? ");
+var idadeComprador = entrada("qual é a sua idade? ");
+
+var idadeComprador = entrada("qual é a sua idade? ");
+
+if(idadeComprador < 18){
+   
+}
