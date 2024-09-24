23463969631b520fcfda937f095e56a664930711
vitoriaeduardaa <167381689+vitoriaeduardaa@users.noreply.github.com>
2024-09-24 23:07:50 +0000

    24/09/2024

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const prompt = require('prompt-sync')
-const entrada = prompt();
+const prompt = require('prompt-sync'); 
+const entrada = prompt(); 
 
-const nomeCompleto = 'Vitoria Eduarda Araujo';
-const anoNascimento = '2006';
-const anoAtual = '2024';
+const nomeCompleto = "Vitoria Eduarda Araujo"; 
+const anoNascimento = 2006;
+let anoAtual = 2024; 
+let idade = 18;
 
+ console.log("Olá eu sou " + nomeCompleto + " estamos no ano de " + anoAtual + " minha idade é " + idade + ".");
 
+let AnoAtual = 2025; 
+let Idade = 18; 
+const NomeCompleto = " Vitoria Eduarda Araujo "; 
+const Faculdade = " nenhuma ";
 
+console.log("Olá meu nome é " + NomeCompleto + ", tenho " + Idade + " anos " + "estamos no ano de " + AnoAtual + " gostaria de fazer faculdade na área de " + Faculdade);
 
+console.log(`oi eu sou ${NomeCompleto} meu ano de nascimento é ${anoNascimento} minha idade é ${idade}`) 
+  
+const listaDeViagens = new Array (
+  `Dubai`, 
+  `Paris`, 
+  `Miami`, 
+  `Chile`,
+  `Toquio`
+);
+console.log(listaDeViagens)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log( listaDeViagens [4])
+listaDeViagens.push("Coronel Vivida"); 
 console.log(listaDeViagens);
 
-var nomeComprador = entrada(" qual é seu nome? ");
-var idadeComprador = entrada("qual é a sua idade? ");
+listaDeViagens.splice(1,2); 
+console.log(listaDeViagens); 
 
-var idadeComprador = entrada("qual é a sua idade? ");
+var nomeComprador = entrada (" qual é seu nome? ");
+var idadeComprador = entrada("qual é sua idade? ");
 
 if(idadeComprador < 18){
-   
-}
+ console.log(`ola sua idade nao corresponde com nossas diretrizes ${nomeComprador}`);
+  }
+if(idadeComprador >= 18){
+  console.log("Parabéns vpcê adiquiriu sua passagem com sucesso!")}
